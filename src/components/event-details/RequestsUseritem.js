b874610266cf4d41c520da1996113c219e90f1b7
Engbashayer <eng.almutairi1991@gmail.com>
2023-12-17 19:33:49 +0300

    par

--- src/components/event-details/RequestsUseritem.js
+++ src/components/event-details/RequestsUseritem.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { PauseCircle } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { getParticipationsbyId } from "../../api/users";
 
-const RequestsUserItem = ({ handleOpenModal }) => {
+const RequestsUserItem = ({ participation, eventId, handleOpenModal }) => {
+  const { data: participationObj } = useQuery({
+    queryKey: ["participationObj", eventId, participation],
+    queryFn: () => getParticipationsbyId(eventId, participation),
+  });
+
+  console.log(participationObj);
+  //get profile user
   return (
     <div
       onClick={handleOpenModal}
