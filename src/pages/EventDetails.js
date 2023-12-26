bfce5c2173518e1b9b7f83a2b070d06d13cde7b2
Moodhyalbannai <mood_92@hotmail.com>
2023-12-26 20:25:55 +0300

    aa

--- src/pages/EventDetails.js
+++ src/pages/EventDetails.js
@@ -1,199 +1,210 @@
-import React, { useState } from "react";
-import { useQuery } from "react-query";
-import CreateEventSearchBar from "../components/create-event/CreateEventSearchBar";
-import DraftEventDetails from "../components/create-event/DraftEventDetails";
-import RequestsAndAccepted from "../components/event-details/RequestsAndAccepted";
-import RequestsUserItem from "../components/event-details/RequestsUseritem";
-import UserProfileModal from "../components/users/UserProfileModal";
-import { getEventById } from "../api/event";
+// import React, { useState } from "react";
+// import { useQuery } from "react-query";
+// import CreateEventSearchBar from "../components/create-event/CreateEventSearchBar";
+// import DraftEventDetails from "../components/create-event/DraftEventDetails";
+// import RequestsAndAccepted from "../components/event-details/RequestsAndAccepted";
+// import RequestsUserItem from "../components/event-details/RequestsUseritem";
+// import UserProfileModal from "../components/users/UserProfileModal";
+// import { getEventById } from "../api/event";
+// import { useParams } from "react-router-dom";
+// import { getAllUsers } from "../api/users";
+// import AcceptedUserItem from "../components/event-details/AcceptedUserItem";
 
-const EventDetails = () => {
-  const { eventId } = useParams();
+// const EventDetails = () => {
+//   const { eventId } = useParams();
 
-  //geteventbyID
+//   //geteventbyID
 
-  const { data: event, isLoading: isLoading } = useQuery({
-    queryKey: ["event"],
-    queryFn: () => getOneEvent(eventId),
-  });
+//   // const { data: event, isLoading: isLoading } = useQuery({
+//   //   queryKey: ["event"],
+//   //   queryFn: () => getOneEvent(eventId),
+//   // });
 
-  const { data: users } = useQuery({
-    queryKey: ["users"],
-    queryFn: () => getAllUsers(),
-  });
-  //console.log(users);
+//   const { data: users } = useQuery({
+//     queryKey: ["users"],
+//     queryFn: () => getAllUsers(),
+//   });
+//   //console.log(users);
 
-  // const { data: par1 } = useQuery({
-  //   queryKey: ["par1"],
-  //   queryFn: () => getParticipationsByUser(userId),
-  // });
-  // console.log("par1");
-  // console.log(par1);
+//   // const { data: par1 } = useQuery({
+//   //   queryKey: ["par1"],
+//   //   queryFn: () => getParticipationsByUser(userId),
+//   // });
+//   // console.log("par1");
+//   // console.log(par1);
 
-  // const parId = event?.volunteer_list[0] || [];
-  // console.log(event?.volunteer_list);
-  // console.log("parId is :");
-  // console.log(parId);
+//   // const parId = event?.volunteer_list[0] || [];
+//   // console.log(event?.volunteer_list);
+//   // console.log("parId is :");
+//   // console.log(parId);
 
-  // const { data: participationObj } = useQuery({
-  //   queryKey: ["participationObj"],
-  //   queryFn: () => getParticipationsbyId(parId),
-  // });
+//   // const { data: participationObj } = useQuery({
+//   //   queryKey: ["participationObj"],
+//   //   queryFn: () => getParticipationsbyId(parId),
+//   // });
 
-  // const { data: participationObj } = useQuery(
-  //   ["participationObj", { parId }], // Set a unique query key based on parId
-  //   () => getParticipationsbyId({ parId }), // Provide the query function
-  //   {
-  //     enabled: !!parId, // Enable the query only when parId is truthy
-  //   }
-  // );
+//   // const { data: participationObj } = useQuery(
+//   //   ["participationObj", { parId }], // Set a unique query key based on parId
+//   //   () => getParticipationsbyId({ parId }), // Provide the query function
+//   //   {
+//   //     enabled: !!parId, // Enable the query only when parId is truthy
+//   //   }
+//   // );
 
-  // console.log("participationObj");
-  // console.log(participationObj?.user);
-  // console.log(participationObj?.status);
+//   // console.log("participationObj");
+//   // console.log(participationObj?.user);
+//   // console.log(participationObj?.status);
 
-  //console.log("event?.volunteer_list[0]");
-  //console.log(event?.volunteer_list[0]);
-  //console.log(event?.volunteer_list);
-  // console.log("event?.volunteer_list[1]");
-  // console.log(event?.volunteer_list[1]);
-  //  const c = await event?.map((el, index) => (
-  //     <RequestsUserItem event={el} key={`participation-${index}`} />
-  //   ));
+//   //console.log("event?.volunteer_list[0]");
+//   //console.log(event?.volunteer_list[0]);
+//   //console.log(event?.volunteer_list);
+//   // console.log("event?.volunteer_list[1]");
+//   // console.log(event?.volunteer_list[1]);
+//   //  const c = await event?.map((el, index) => (
+//   //     <RequestsUserItem event={el} key={`participation-${index}`} />
+//   //   ));
 
-  const [requests, setRequests] = useState(true);
-  const [accepted, setAccepted] = useState(false);
+//   const [requests, setRequests] = useState(true);
+//   const [accepted, setAccepted] = useState(false);
+//   const [showUserProfileModal, setShowUserProfileModal] = useState(false);
+//   const [acceptedUserShow, setAcceptedUserShow] = useState(false);
+//   const [rejectedUserShow, setRejectedUserShow] = useState(false);
 
-  const [participationId, setParticipationId] = useState(null);
+//   const [participationId, setParticipationId] = useState(null);
 
-  // Example useQuery for fetching event details
-  const { data: currentEvent } = useQuery(
-    ["event", currentEventId],
-    fetchEventById
-  );
+//   // Example useQuery for fetching event details
+//   // const { data: currentEvent } = useQuery(
+//   //   ["event", currentEventId],
+//   //   fetchEventById
+//   // );
 
-  const { data: eventById, isLoading: isLoading1 } = useQuery({
-    queryKey: ["eventById"],
-    queryFn: () => getEventById(eventId),
-  });
-  const listOfParticipations = eventById?.volunteer_list;
+//   const { data: eventById, isLoading: isLoading1 } = useQuery({
+//     queryKey: ["eventById"],
+//     queryFn: () => getEventById(eventId),
+//   });
+//   const listOfParticipations = eventById?.volunteer_list;
 
-  // listOfParticipations?.map((e) => {
-  //   console.log(e.user);
-  // });
+//   // listOfParticipations?.map((e) => {
+//   //   console.log(e.user);
+//   // });
 
-  console.log(listOfParticipations);
+//   console.log(listOfParticipations);
 
-  // const { data: parById, isLoading: isLoading2 } = useQuery({
-  //   queryKey: ["parById"],
+//   // const { data: parById, isLoading: isLoading2 } = useQuery({
+//   //   queryKey: ["parById"],
 
-  //   queryFn: () => getParticipationsById(eventId),
-  // });
+//   //   queryFn: () => getParticipationsById(eventId),
+//   // });
 
-  // console.log(promises);
+//   // console.log(promises);
 
-  const handleAcceptedClick = () => {
-    setRequests(true);
-    setAccepted(false);
-  };
+//   const handleAcceptedClick = () => {
+//     setRequests(true);
+//     setAccepted(false);
+//   };
 
-  // Handle opening and closing user profile modal
-  const handleCloseModal = () => {
-    setShowUserProfileModal(false);
-  };
+//   const handleRequestsClick = () => {
+//     setRequests(false);
+//     setAccepted(true);
+//   };
 
-  const handleOpenModal = (participationId) => {
-    setShowUserProfileModal(true);
-    setParticipationId(participationId);
-  };
+//   // Handle opening and closing user profile modal
+//   const handleCloseModal = () => {
+//     setShowUserProfileModal(false);
+//   };
 
-  const handleRejectedUser = () => {
-    // Your logic for handling rejected user
-  };
+//   const handleOpenModal = (participationId) => {
+//     setShowUserProfileModal(true);
+//     setParticipationId(participationId);
+//   };
 
-  const handleAcceptedUser = () => {
-    // Your logic for handling accepted user
-  };
+//   const handleRejectedUser = () => {
+//     // Your logic for handling rejected user
+//   };
 
-  // console.log("volunteer listttttt num 1");
-  // console.log(event?.volunteer_list[0]?._id?.status);
-  // //undefind
-  // const c = event?.volunteer_list.map((participation) => (
-  //   <RequestsUserItem id={_id} key={`event-${index}`} />
-  // ));
+//   const handleAcceptedUser = () => {
+//     // Your logic for handling accepted user
+//   };
 
-  return (
-    <div className="min-w-screen h-screen bg-NavyMain lg:px-[100px]">
-      <div className="h-[100vh] mt-[80px] lg:grid lg:grid-cols-2 flex flex-col">
-        <div className="h-full md:min-h-screen p-8 md:pt-[100px] ">
-          <div className="w-full h-full lg:px-[50px] flex flex-col text-center gap-4 bg-white rounded-lg p-4">
-            <DraftEventDetails eventById={eventById} />
-          </div>
-        </div>
-        <div className="bg-NavyMain h-full md:min-h-screen p-8 lg:p-11 flex flex-col gap-6 items-center">
-          <h1 className="flex gap-2 text-white font-normal text-2xl">
-            Small Act,
-            <span className="font-bold">Big Impact</span>
-          </h1>
-          <CreateEventSearchBar />
-          <RequestsAndAccepted
-            requests={requests}
-            accepted={accepted}
-            handleAcceptedClick={handleAcceptedClick}
-            handleRequestsClick={handleRequestsClick}
-          />
-          {requests ? (
-            <div className=" w-full h-full flex flex-col overflow-y-scroll overflow-hidden no-scrollbar">
-              <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-3">
-                {listOfParticipations?.map((el) => (
-                  <RequestsUserItem
-                    key={el._id}
-                    participation={el}
-                    handleOpenModal={handleOpenModal}
-                  />
-                ))}
+//   // console.log("volunteer listttttt num 1");
+//   // console.log(event?.volunteer_list[0]?._id?.status);
+//   // //undefind
+//   // const c = event?.volunteer_list.map((participation) => (
+//   //   <RequestsUserItem id={_id} key={`event-${index}`} />
+//   // ));
 
-                {event?.volunteer_list?.map((el, index) => (
-                  <RequestsUserItem
-                    eventId={eventId}
-                    participation={el}
-                    key={`event-${index}`}
-                  />
-                ))}
+//   return (
+//     <div className="min-w-screen h-screen bg-NavyMain lg:px-[100px]">
+//       <div className="h-[100vh] mt-[80px] lg:grid lg:grid-cols-2 flex flex-col">
+//         <div className="h-full md:min-h-screen p-8 md:pt-[100px] ">
+//           <div className="w-full h-full lg:px-[50px] flex flex-col text-center gap-4 bg-white rounded-lg p-4">
+//             <DraftEventDetails eventById={eventById} />
+//           </div>
+//         </div>
+//         <div className="bg-NavyMain h-full md:min-h-screen p-8 lg:p-11 flex flex-col gap-6 items-center">
+//           <h1 className="flex gap-2 text-white font-normal text-2xl">
+//             Small Act,
+//             <span className="font-bold">Big Impact</span>
+//           </h1>
+//           <CreateEventSearchBar />
+//           <RequestsAndAccepted
+//             requests={requests}
+//             accepted={accepted}
+//             handleAcceptedClick={handleAcceptedClick}
+//             handleRequestsClick={handleRequestsClick}
+//           />
+//           {requests ? (
+//             <div className=" w-full h-full flex flex-col overflow-y-scroll overflow-hidden no-scrollbar">
+//               <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-3">
+//                 {listOfParticipations?.map((el) => (
+//                   <RequestsUserItem
+//                     key={el._id}
+//                     participation={el}
+//                     handleOpenModal={handleOpenModal}
+//                   />
+//                 ))}
 
-                {/*     event={el} key={`organization-${index}`}
-                 <RequestsUserItem handleOpenModal={handleOpenModal} />
-                <RequestsUserItem handleOpenModal={handleOpenModal} />
-                <RequestsUserItem handleOpenModal={handleOpenModal} />
-                <RequestsUserItem handleOpenModal={handleOpenModal} />
-                <RequestsUserItem handleOpenModal={handleOpenModal} />
-                <RequestsUserItem handleOpenModal={handleOpenModal} />
-                <RequestsUserItem handleOpenModal={handleOpenModal} />
-                <RequestsUserItem handleOpenModal={handleOpenModal} />
-                <RequestsUserItem handleOpenModal={handleOpenModal} />
-                <RequestsUserItem handleOpenModal={handleOpenModal} />
-                <RequestsUserItem handleOpenModal={handleOpenModal} /> */}
-              </div>
-            </div>
-          ) : (
-            <div className=" w-full h-full flex flex-col overflow-y-scroll overflow-hidden no-scrollbar">
-              <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-3">
-                <AcceptedUserItem handleOpenModal={handleOpenModal} />;
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-      <UserProfileModal
-        showUserProfileModal={showUserProfileModal}
-        handleAcceptedUser={handleAcceptedUser}
-        acceptedUserShow={acceptedUserShow}
-        rejectedUserShow={rejectedUserShow}
-        handleCloseModal={handleCloseModal}
-        handleRejectedUser={handleRejectedUser}
-      />
-    </div>
-  );
-};
+//                 {/* {event?.volunteer_list?.map((el, index) => (
+//                   <RequestsUserItem
+//                     eventId={eventId}
+//                     participation={el}
+//                     key={`event-${index}`}
+//                   />
+//                 ))} */}
 
-export default EventDetails;
+//                 {/*     event={el} key={`organization-${index}`}
+//                  <RequestsUserItem handleOpenModal={handleOpenModal} />
+//                 <RequestsUserItem handleOpenModal={handleOpenModal} />
+//                 <RequestsUserItem handleOpenModal={handleOpenModal} />
+//                 <RequestsUserItem handleOpenModal={handleOpenModal} />
+//                 <RequestsUserItem handleOpenModal={handleOpenModal} />
+//                 <RequestsUserItem handleOpenModal={handleOpenModal} />
+//                 <RequestsUserItem handleOpenModal={handleOpenModal} />
+//                 <RequestsUserItem handleOpenModal={handleOpenModal} />
+//                 <RequestsUserItem handleOpenModal={handleOpenModal} />
+//                 <RequestsUserItem handleOpenModal={handleOpenModal} />
+//                 <RequestsUserItem handleOpenModal={handleOpenModal} /> */}
+//               </div>
+//             </div>
+//           ) : (
+//             <div className=" w-full h-full flex flex-col overflow-y-scroll overflow-hidden no-scrollbar">
+//               <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-3">
+//                 <AcceptedUserItem handleOpenModal={handleOpenModal} />;
+//               </div>
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//       <UserProfileModal
+//         showUserProfileModal={showUserProfileModal}
+//         handleAcceptedUser={handleAcceptedUser}
+//         acceptedUserShow={acceptedUserShow}
+//         rejectedUserShow={rejectedUserShow}
+//         handleCloseModal={handleCloseModal}
+//         handleRejectedUser={handleRejectedUser}
+//       />
+//     </div>
+//   );
+// };
+
+// export default EventDetails;
